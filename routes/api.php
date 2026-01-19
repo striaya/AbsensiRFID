2bcbf5f61925a0aeae38263227186eaee692117c
striaya <shenggaryoselino@gmail.com>
2026-01-19 08:25:54 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AbsensiController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/absensi', [AbsensiController::class, 'store']);
