99baf72ed0994316efd45983c1d53ccf5b039bcd
striaya <shenggaryoselino@gmail.com>
2026-01-23 21:52:17 +0700

    AbsensiRFID

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -12,4 +12,79 @@ class GuruController extends Controller
             'data' => Guru::all()
         ]);
     }
+
+    public function store() {
+        $request->validate([
+            'nama' => 'required|string',
+            'nip' => 'required|unique:gurus',
+            'email' => 'required|email|unique:gurus'
+        ]);
+
+        $guru = Guru::create($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Guru berhasil ditambahkan',
+            'data' => $guru
+        ], 201);
+    }
+
+    public function show($id) {
+        $guru = Guru::find($id);
+
+        if(!$guru) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Guru tidak ditemukan'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $guru
+        ]);
+    } 
+
+    public function update(Request $request, $id){
+        $guru = Guru::find($id);
+
+        if(!$guru) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Guru tidak ditemukan'
+            ], 404);
+        }
+
+        $request->validate([
+            'nama' => 'required',
+            'nip' => 'required|unique:gurus,nip,' .$id,
+            'email' => 'required|email|enique:gurus,email, ' .$id
+        ]);
+
+        $guru->update($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Guru berhasil diupdate',
+            'data' => $guru
+        ]);
+    }
+
+    public function destroy($id){
+        $guru = Guru::find($id);
+
+        if(!$guru) {
+            return response()->json([
+                'success' => false,
+                'message' => "Guru tidak ditemukan"
+            ], 404);
+        }
+
+        $guru->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Guru berhasil dihapus'
+        ]);
+    }
 }
