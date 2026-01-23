9342924f8b4cd4299c51083df726ebd4db6a459c
striaya <shenggaryoselino@gmail.com>
2026-01-23 23:30:43 +0700

    AbsensiRFID

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\GuruController;
 use App\Http\Controllers\AbsensiController;
+use App\Http\Controllers\DeviceController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/absensi', [AbsensiController::class, 'store']);
 
 Route::apiResource('/guru', GuruController::class);
 Route::get('/guru', [GuruController::class, 'index']);
+
+Route::apiResource('/device', DeviceController::class);
+Route::get('/device', [DeviceController::class, 'index']);
